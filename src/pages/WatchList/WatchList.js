4f086b1d2948f0344842c72c0e95af94470e16d7
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-06-10 00:27:22 +0200

    Finish watch list page

--- src/pages/WatchList/WatchList.js
+++ src/pages/WatchList/WatchList.js
@@ -1,8 +1,80 @@
+import axios from 'axios'
+import CryptocurrenciesList from 'components/CryptocurrenciesList/CryptocurrenciesList'
+import CoinDetail from 'components/CoinDetail/CoinDetail'
+import { useAppContext } from 'context/AppContext'
+import { useQuery } from 'react-query'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Wrapper, Container } from './WatchListStyled'
+
+const fetchCryptocurrencies = async (currency, watchList) => {
+  const response = await axios(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${watchList}&order=market_cap_desc`
+  )
+  return response.data
+}
+
 const WatchList = () => {
+  const { watchList, currency } = useAppContext()
+  const { isLoading, isError, error, data } = useQuery(
+    ['watchListCrypto', watchList],
+    () => fetchCryptocurrencies(currency, watchList)
+  )
+
+  const notifyAdd = (coinName) =>
+    toast.success(`${coinName} added to watch list`, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+
+  const notifyDelete = (coinName) =>
+    toast.error(`${coinName} was deleted from watch list`, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Wrapper>
+      <Container>
+        <ToastContainer
+          position='top-center'
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+        />
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : isError ? (
+          <div>Error: {error.message}</div>
+        ) : (
+          <>
+            <CryptocurrenciesList />
+            {data.map((coin) => (
+              <CoinDetail
+                key={coin.id}
+                coin={coin}
+                notifyAdd={notifyAdd}
+                notifyDelete={notifyDelete}
+              />
+            ))}
+          </>
+        )}
+      </Container>
+    </Wrapper>
   )
 }
 
--- src/pages/WatchList/WatchListStyled.js
+++ src/pages/WatchList/WatchListStyled.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components/macro'
 
-export const Container = styled.div``
+export const Wrapper = styled.main`
+  max-width: 1200px;
+  margin: 100px auto;
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`

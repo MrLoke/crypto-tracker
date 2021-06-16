5de668a4cc3e0709d05f982c396c0ed0f30e7037
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-06-16 02:02:02 +0200

    Remove ReactQueryDevtools

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import Root from 'pages/Root'
 import { ThemeModeProvider } from 'context/ThemeContext'
 import { AppCtxProvider } from 'context/AppContext'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       <ThemeModeProvider>
         <AppCtxProvider>
           <Root />
